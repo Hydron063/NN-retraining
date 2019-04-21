a7626521344ac40853f99c7185e09391594ce39c
Antoine <hydron63@gmail.com>
2019-04-21 19:49:00 +0300

    Update prepare.py

--- training/prepare.py
+++ training/prepare.py
@@ -163,8 +163,9 @@ def full_prep(step1=True,step2 = True):
             print(os.path.exists(f))
             print(os.path.exists('./detector/labels/label_job5.csv'))
             print(os.path.exists('/Rage/training/detector/labels/label_job5.csv'))
-            print(os.getcwd(), 'sdad')
-            print('aaa')
+            print(os.getcwd(), '1')
+            os.fchdir('./../)
+            print(os.getcwd(), '2')
             input()
             content = np.array(pandas.read_csv(f))
             content = content[content[:,0]!=np.nan]
