0eaad44f675b3654fa348e066ee7829be1f192d8
Antoine <hydron63@gmail.com>
2019-05-01 22:49:02 +0300

    Update config_training.py

--- training/config_training.py
+++ training/config_training.py
@@ -7,11 +7,7 @@ config = {'stage1_data_path':'./../drive/My Drive/work/DataBowl3/stage2/stage2',
           
           'luna_abbr':'./detector/labels/shorter.csv',
           'luna_label':'./detector/labels/lunaqualified.csv',
-          'stage1_annos_path':['./detector/labels/label_job5.csv',
-                './detector/labels/label_job4_2.csv',
-                './detector/labels/label_job4_1.csv',
-                './detector/labels/label_job0.csv',
-                './detector/labels/label_qualified.csv'],
+          'stage1_annos_path':['./detector/labels/etiquettes.csv'],
           'bbox_path':'../detector/results/res18/bbox/',
           'preprocessing_backend':'python'
          }
