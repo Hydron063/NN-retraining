2a7aa9bb25068524fc9b07b16c32feba1befcbc1
Antoine <hydron63@gmail.com>
2019-05-02 20:48:15 +0300

    Update data.py

--- training/detector/data.py
+++ training/detector/data.py
@@ -61,7 +61,7 @@ class DataBowl3Detector(Dataset):
                     print('Apres', self.bboxes)
                 else:
                     print("Diable", i, l)
-            self.bboxes = np.concatenate(self.bboxes,axis = 0
+            self.bboxes = np.concatenate(self.bboxes,axis = 0)
 
         self.crop = Crop(config)
         self.label_mapping = LabelMapping(config, self.phase)
