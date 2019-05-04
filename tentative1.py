e61e584789d83e0739d5b55c4dab7686dde9f3c9
Antoine <hydron63@gmail.com>
2019-05-04 05:39:08 +0300

    Create tentative1.py

---
+++ tentative1.py
@@ -0,0 +1,156 @@
+from preprocessing import full_prep
+from config_submit import config as config_submit
+
+import torch
+from torch.nn import DataParallel
+from torch.backends import cudnn
+from torch.utils.data import DataLoader
+from torch import optim
+from torch.autograd import Variable
+
+from layers import acc
+from data_detector import DataBowl3Detector,collate
+from data_classifier import DataBowl3Classifier
+
+from utils import *
+from split_combine import SplitComb
+from test_detect import test_detect
+from importlib import import_module
+import pandas
+
+parser = argparse.ArgumentParser(description='PyTorch DataBowl3 Detector')
+parser.add_argument('--model1', '-m1', metavar='MODEL', default='net_detector',
+                    help='model')
+parser.add_argument('--model2', '-m2', metavar='MODEL', default='net_classifier',
+                    help='model')
+parser.add_argument('-j', '--workers', default=32, type=int, metavar='N',
+                    help='number of data loading workers (default: 32)')
+parser.add_argument('--epochs', default=None, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=16, type=int,
+                    metavar='N', help='mini-batch size (default: 16)')
+parser.add_argument('-b2', '--batch-size2', default=3, type=int,
+                    metavar='N', help='mini-batch size (default: 16)')
+parser.add_argument('--lr', '--learning-rate', default=None, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--save-freq', default='5', type=int, metavar='S',
+                    help='save frequency')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--save-dir', default='', type=str, metavar='SAVE',
+                    help='directory to save checkpoint (default: none)')
+parser.add_argument('--test1', default=0, type=int, metavar='TEST',
+                    help='do detection test')
+parser.add_argument('--test2', default=0, type=int, metavar='TEST',
+                    help='do classifier test')
+parser.add_argument('--test3', default=0, type=int, metavar='TEST',
+                    help='do classifier test')
+parser.add_argument('--split', default=8, type=int, metavar='SPLIT',
+                    help='In the test phase, split the image to 8 parts')
+parser.add_argument('--gpu', default='all', type=str, metavar='N',
+                    help='use gpu')
+parser.add_argument('--n_test', default=8, type=int, metavar='N',
+                    help='number of gpu for test')
+parser.add_argument('--debug', default=0, type=int, metavar='TEST',
+                    help='debug mode')
+parser.add_argument('--freeze_batchnorm', default=0, type=int, metavar='TEST',
+help='freeze the batchnorm when training')
+
+args = parser.parse_args()
+
+
+datapath = config_submit['datapath']
+prep_result_path = config_submit['preprocess_result_path']
+skip_prep = config_submit['skip_preprocessing']
+skip_detect = config_submit['skip_detect']
+
+nodmodel = import_module(args.model1)
+config1, nod_net, loss, get_pbb = nodmodel.get_model()
+checkpoint = torch.load(config_submit['detector_param'])
+nod_net.load_state_dict(checkpoint['state_dict'])
+
+torch.cuda.set_device(0)
+nod_net = nod_net.cuda()
+cudnn.benchmark = True
+nod_net = DataParallel(nod_net)
+
+if not skip_prep:
+    testsplit = full_prep(datapath,prep_result_path,
+                          n_worker = config_submit['n_worker_preprocessing'],
+                          use_existing=config_submit['use_exsiting_preprocessing'])
+else:
+    testsplit = os.listdir(datapath)
+
+net.forward(prep_result_path)
+
+# ???
+
+casemodel = import_module(args.model2)
+config2 = casemodel.config
+args.lr_stage2 = config2['lr_stage']
+args.lr_preset2 = config2['lr']
+topk = config2['topk']
+case_net = casemodel.CaseNet(topk=topk, nodulenet=nod_net)
+args.miss_ratio = config2['miss_ratio']
+args.miss_thresh = config2['miss_thresh']
+
+
+
+
+print(save_dir)
+print(args.save_freq)
+# Les noms des dossiers avec les images (affectes manuellement, a corriger)
+trainsplit = np.array(['LUNGx-CT001'])
+valsplit = np.array(['LUNGx-CT001'])
+testsplit = np.array(['LUNGx-CT001'])
+
+trainsplit = np.load('full.npy')
+dataset = DataBowl3Classifier(trainsplit,config2,phase = 'train')
+train_loader_case = DataLoader(dataset,batch_size = args.batch_size2,
+    shuffle = True,num_workers = args.workers,pin_memory=True)
+
+dataset = DataBowl3Classifier(valsplit,config2,phase = 'val')
+val_loader_case = DataLoader(dataset,batch_size = max([args.batch_size2,1]),
+    shuffle = False,num_workers = args.workers,pin_memory=True)
+
+dataset = DataBowl3Classifier(trainsplit,config2,phase = 'val')
+all_loader_case = DataLoader(dataset,batch_size = max([args.batch_size2,1]),
+    shuffle = False,num_workers = args.workers,pin_memory=True)
+
+optimizer2 = torch.optim.SGD(case_net.parameters(),
+    args.lr,momentum = 0.9,weight_decay = args.weight_decay)
+
+
+
+for epoch in range(start_epoch, end_epoch + 1):
+    if epoch ==start_epoch:
+        lr = args.lr
+        debug = args.debug
+        args.lr = 0.0
+        args.debug = True
+        train_casenet(epoch,case_net,train_loader_case,optimizer2,args)
+        args.lr = lr
+        args.debug = debug
+    #
+    if epoch>config2['startepoch']:
+        train_casenet(epoch,case_net,train_loader_case,optimizer2,args)
+        val_casenet(epoch,case_net,val_loader_case,args)
+        val_casenet(epoch,case_net,all_loader_case,args)
+
+    if epoch % args.save_freq == 0:            
+        state_dict = case_net.module.state_dict()
+        for key in state_dict.keys():
+            state_dict[key] = state_dict[key].cpu()
+
+        torch.save({
+            'epoch': epoch,
+            'save_dir': save_dir,
+            'state_dict': state_dict,
+            'args': args},
+            os.path.join(save_dir, '%03d.ckpt' % epoch))
